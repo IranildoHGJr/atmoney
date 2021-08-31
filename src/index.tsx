705fd517f0431212bb5bf8a6c397b97d5e9f7acd
Iranildo <iranildojr.11@gmail.com>
2021-08-31 20:13:40 -0300

    teste

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './App';// Pode renomiar com "as" exemplo : import { App as Batata } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
