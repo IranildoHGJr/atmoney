b6d48f0a6a0b214ce7b79d20a85c8538c6329ff1
Iranildo <iranildojr.11@gmail.com>
2021-09-03 19:55:18 -0300

    Aula5

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-  button{
-    background: #000;
-  }
-`
-
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-        <Title>
-          Hello World
-          <button></button>
-        </Title>
+          <h1>Hello World</h1>
+          <GlobalStyle />
     </div>
   );
 }
