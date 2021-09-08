bdc460bee292922bb86eeb138be50586ba7d6a6f
Iranildo <iranildojr.11@gmail.com>
2021-09-08 17:10:18 -0300

    Cap2SubCap4Part2

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { useState } from 'react';
 
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
 
@@ -22,12 +23,10 @@ export function App() {
           <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
           <Dashboard />
 
-          <Modal 
-            isOpen={isNewTransactionModalOpen} 
+          <NewTransactionModal 
+            isOpen={isNewTransactionModalOpen}
             onRequestClose={handleCloseNewTransactionModal}
-          >
-            <h2>Cadastrar transação</h2>
-          </Modal>
+          />
 
           <GlobalStyle />
     </>
